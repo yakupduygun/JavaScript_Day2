d71d4dcb413f2f1234d84536ecbf2ac820696c90
ajnacodes <b.daniela196@outlook.com>
2022-05-30 10:57:11 +0200

    basic1 done

---
+++ Daniela/basic.js
@@ -0,0 +1,6 @@
+// Basic exercise 1
+
+function crystalGazer(job_title, location, partners_name, number_childrens) {
+    return ("You will be a " + job_title + " in " + location + " and married to  " + partners_name + " with " + number_childrens + " children.")
+}
+console.log(crystalGazer("Developer", "Vienna", "no one", "zero"))
